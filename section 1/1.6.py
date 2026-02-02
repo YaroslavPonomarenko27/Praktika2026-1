d07ec7e1d707139ab1f8da09607a1aa5f9c159bd
ЯрославПономаренко27 <yaroslavspartanets@gmail.com>
2026-02-03 00:36:10 +0200

    commits

--- section 1/1.6.py
+++ section 1/1.6.py
@@ -1,4 +1,14 @@
-age = int(input("Вік: "))
+# Звичайна функція
+def add(a, b):
+    return a + b
 
-if age >= 18 and age <= 65:
-    print("Працездатний вік")
+result = add(5, 3)
+print("Сума:", result)
+
+# Рекурсивна функція
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+print("Факторіал 5:", factorial(5))
