dbc0d353a8d3e7ece7281aaa2d37cf2a5c221283
ЯрославПономаренко27 <yaroslavspartanets@gmail.com>
2026-02-03 00:38:31 +0200

    commit

--- section 1/1.7.py
+++ section 1/1.7.py
@@ -1,2 +1,20 @@
-for i in range(1, 6):
-    print(i)
+# Список
+numbers = [1, 2, 3]
+
+# Кортеж
+point = (10, 20)
+
+# Множина
+colors = {"red", "green", "blue"}
+
+# Словник
+student = {
+    "name": "Марія",
+    "age": 20
+}
+
+print(numbers)
+print(point)
+print(colors)
+print(student)
+
